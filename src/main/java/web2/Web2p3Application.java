04ce6855baba3ae956994f058c0701565532882c
Cesar A. Garcia <mrcesar9402@gmail.com>
2016-10-04 00:55:50 -0400

    #8, #9 terminados basicamente, solo agregar mas mensajes y mas archivos con templates

--- src/main/java/web2/Web2p3Application.java
+++ src/main/java/web2/Web2p3Application.java
@@ -2,11 +2,31 @@ package web2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class Web2p3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Web2p3Application.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		return slr;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasenames("mensajes");  // name of the resource bundle
+		source.setUseCodeAsDefaultMessage(true);
+		return source;
 	}
 }
